bd2de5b0b489cce033c1c73e95451c01e6d9e6c3
David Kelly <sierro@Davids-MacBook-Air.local>
2026-03-29 19:38:16 +0100

    chore: update BUILD_ID to gauntlet-db

--- js/00_config.js
+++ js/00_config.js
@@ -6,7 +6,7 @@
  *   transformed along with the map.
  */
 
-const BUILD_ID = '2026-03-29.gauntlet-fix';
+const BUILD_ID = '2026-03-29.gauntlet-db';
 (function(){ var el = document.getElementById('buildBadge'); if (el) el.textContent = BUILD_ID; })();
 
 // ---- Config ----
