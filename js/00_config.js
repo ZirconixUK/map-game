817def2992d8bd7389957b59f29bb3945be94a1d
David Kelly <sierro@Davids-MacBook-Air.local>
2026-04-02 14:39:36 +0100

    feat: add Remote mode button, move counter HUD, undo button, load 22_remote.js

--- js/00_config.js
+++ js/00_config.js
@@ -6,7 +6,7 @@
  *   transformed along with the map.
  */
 
-const BUILD_ID = '2026-04-02.remote-state';
+const BUILD_ID = '2026-04-02.remote-html';
 (function(){ var el = document.getElementById('buildBadge'); if (el) el.textContent = BUILD_ID; })();
 
 // ---- Config ----
